fc128ecd40c4b8b532c302e8095af4aadb841393
sameermkhan@hotmail.com <Shavane1!>
2020-09-25 03:33:54 -0400

    Added a function in Main.java to allow the 2 items in Listview when clicked, to display in the bottom fragment.

--- app/src/main/java/com/sameerkhan_ceng319lab01_ex1/MainActivity.java
+++ app/src/main/java/com/sameerkhan_ceng319lab01_ex1/MainActivity.java
@@ -3,9 +3,12 @@ package com.sameerkhan_ceng319lab01_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +22,23 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         String[] activity = {"AIActivity", "VRActivity"};
 
-        ListView listView=(ListView)findViewById(R.id.lst);
+        final ListView listView=(ListView)findViewById(R.id.lst);
         ArrayAdapter<String> adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,activity);
 
         listView.setAdapter(adapter);
 
-    }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Object o = listView.getItemAtPosition(position);
+                String str=(String)o;//As you are using Default String Adapter
+                TextView textView = (TextView)findViewById(R.id.textView);
+                textView.setText(str); //set text for text view
+            }
+        });
+
+}
     @Override
     protected void onStart() {
         super.onStart();
